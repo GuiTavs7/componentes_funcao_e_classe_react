cb7c5be4c5242795b0fa6c8c474e05bc901bb856
GuiTavs7 <7tavs7@gmail.com>
2025-02-22 19:03:13 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Card from "./components/card/card"
+//import Button from "./components/button/button"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </>
+  )
+}
+
+export default App
